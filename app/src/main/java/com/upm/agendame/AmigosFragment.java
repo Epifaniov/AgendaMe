bf9f777d9a25fab7532b0cff2433ee0e1951f53a
gustyAdolf <36839936+gustyAdolf@users.noreply.github.com>
2019-06-08 20:09:37 +0200

    Fragment Amigos, muestra los amigos

--- app/src/main/java/com/upm/agendame/AmigosFragment.java
+++ app/src/main/java/com/upm/agendame/AmigosFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +23,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class AmigosFragment extends Fragment {
 
@@ -29,6 +31,7 @@ public class AmigosFragment extends Fragment {
     private JsonObjectRequest jsonObjectRequest;
     private RecyclerView recyclerView;
     private Usuario usrO;
+    private List<Usuario> amigos;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_amigos, container,false);
@@ -37,7 +40,8 @@ public class AmigosFragment extends Fragment {
         recyclerView=(RecyclerView) v.findViewById(R.id.recyContacts);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         usrO=(Usuario)getActivity().getIntent().getExtras().getSerializable("usuario");
-
+        amigos = new ArrayList<>();
+        cargarAmigos();
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
@@ -46,8 +50,9 @@ public class AmigosFragment extends Fragment {
 
             @Override
             public boolean onQueryTextChange(String s) {
-                String url = getActivity().getString(R.string.ip) + "SearchUsers.php?nombre=" + s;
+                String url = getActivity().getString(R.string.ip) + "SearchUsers.php?id="+usrO.getId()+"&nombre=" + s;
                 url.replace(" ", "%20");
+                Log.d("URLSearch",url);
                 jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
@@ -87,6 +92,45 @@ public class AmigosFragment extends Fragment {
             }
         });
         return v;
+    }
+
+
+    private void cargarAmigos(){
+        String url=getString(R.string.ip)+"GetFriends.php?id="+usrO.getId()+"&nombre=";
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+            JSONArray json = response.optJSONArray("amigos");
+                try {
+            for(int i=0; i<json.length();i++){
+                Usuario usr = new Usuario();
+                JSONObject jsonObject = json.getJSONObject(i);
+                usr.setId(jsonObject.optString("ID"));
+                usr.setNombre(jsonObject.optString("NOMBRE"));
+                usr.setRuta_img(jsonObject.optString("RUTA_IMAGEN"));
+                usr.setEmail(jsonObject.optString("EMAIL"));
+                usr.setTelefono(jsonObject.optString("TELEFONO"));
+                usr.setNum_citas(jsonObject.optString("NUM_CITAS"));
+                usr.setNum_citas(jsonObject.optString("NUM_AMIGOS"));
+                amigos.add(usr);
+
+            }
+
+                    SearchUsersAdapter adapter = new SearchUsersAdapter((ArrayList)amigos, getContext(),usrO);
+                    recyclerView.setAdapter(adapter);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        VolleySingleton.getInstanciaVolley(getContext()).addToRequestQueue(jsonObjectRequest);
+
     }
 
 }
